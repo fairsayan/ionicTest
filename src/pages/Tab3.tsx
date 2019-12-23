0ec823e88d8efa09930550b9656e6c8615419515
Domenico Pontari <fairsayan@gmail.com>
2019-12-23 16:55:02 +0100

    first commit

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,17 +1,45 @@
 import React from 'react';
-import { IonHeader, IonToolbar, IonPage, IonTitle, IonContent } from '@ionic/react';
+import { IonHeader, IonToolbar, IonPage, IonTitle, IonContent, withIonLifeCycle } from '@ionic/react';
+import * as $ from "jquery"
 
-const Tab3Page: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab Three</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent />
-    </IonPage>
-  );
-};
+interface IProps {
+}
 
-export default Tab3Page;
+interface IState {
+  name: string;
+}
+
+class Tab3Page extends React.Component<IProps, IState> {
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      name: 'undefined'
+    }
+  }
+
+  ionViewWillEnter() {
+    $.get('https://springbot-demo.herokuapp.com/api/v1/persons/2', (data) => {
+      this.setState({name: data.name})
+    })
+  }
+
+
+  render() {
+    var name:String = this.state.name
+    console.info(this.state)
+
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Tab Three</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">ciao <b>{ name }</b></IonContent>
+      </IonPage>
+    )
+  }
+}
+
+export default withIonLifeCycle(Tab3Page);
